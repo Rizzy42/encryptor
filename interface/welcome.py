6532b8779e533b69014c6f44fd4a14cf42ea0398
Shaurya Rishi <shaurya.rishi@yahoo.com>
2022-04-30 15:57:21 +0530

    Fixed Docker Universal Display Settings

--- interface/welcome.py
+++ interface/welcome.py
@@ -6,7 +6,7 @@ from platform import platform
 
 def welcomeMsg():
 	user_platform = platform()
-	if user_platform == "Linux-5.10.104-linuxkit-x86_64-with":
+	if user_platform == "Linux-5.16.19-76051619-generic-x86_64-with":
 		user_platform = "Docker Universal"
 
 	print(f"\nWelcome to {cyan}Encryptor{white} (Version {cyan}{version}{white}) on {cyan}{user_platform}{white}")
